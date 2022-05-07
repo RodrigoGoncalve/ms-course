de663efc8c5f12a885809eb0a55446cf2a19adef
Rodrigo Goncalves <ro.goncalve@gmail.com>
2022-05-07 15:51:14 -0300

    Private GIT repository

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResoucer.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResoucer.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
-
 
 @RefreshScope
 @RestController
