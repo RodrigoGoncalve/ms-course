10b31d727f60232c479f52cc6f36f3a565f1cbcd
Rodrigo Goncalves <ro.goncalve@gmail.com>
2022-05-09 20:23:33 -0300

    UserFeingClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feingclients.UserFeingClient;
+
+
+
+@Service
+public class UserService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeingClient userFeingClient;
+	
+	public User findByEmail(@RequestParam String email){
+		User user = userFeingClient.findByEmail(email).getBody();
+		if(user == null) {
+			logger.error("Email not found: "+ email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: "+ email);
+		return user;
+	}
+}
